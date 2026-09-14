c9fbd34011f7a4aab6e2351b9afe985cc48d7350
myacoubi <amineamoune904@gmail.com>
2026-09-14 23:43:05 +0100

    fix: rm from audit service contextLoads tests since it requires conection to euruka

--- backend/audit-service/src/test/java/com/buy01/audit/AuditApplicationTests.java
+++ backend/audit-service/src/test/java/com/buy01/audit/AuditApplicationTests.java
@@ -1,13 +1,4 @@
 package com.buy01.audit;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class AuditApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
